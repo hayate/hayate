af59f41f663dcfc53a087e2d09b7f1a422a83154
Andrea Belvedere <andrea@byteset.com>
2010-04-25 08:27:17 +0900

    removing app and htdocs from hayate repo, refactoring directories

--- app/modules/default/views/index.html.php
+++
@@ -1,6 +0,0 @@
-<?= $name ?>
-<form action="/" method="post">
-  <label for="name">Name: </label>
-  <input type="text" name="name" value="<?php echo $name ?>" />
-  <input type="submit" value="Submit" />
-</form>
